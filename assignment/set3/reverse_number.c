afeba1e770266290bbb73c1276bd29a3f8e19db7
kalyanikedasu <kalyanikedasu3@gmail.com>
2025-03-16 16:21:43 +0530

    test

---
+++ assignment/set3/reverse_number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n,r,rev=0;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		r=n%10;
+		rev=rev*10+r;
+		n=n/10;
+	printf("The reversed number is:%d \n",rev);
+	}
+}
+
+
