b35966aa96a42cf1dc082b05d8e8c005a889bf50
kalyanikedasu <kalyanikedasu3@gmail.com>
2025-03-17 11:32:32 +0530

    test

---
+++ assignment/set3/upper_lower_case.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	char i,j;
+       for(i=65;i<=90;i++)
+       {
+        printf("\nUpper case alphabets are:%c\n",i);
+       }
+       printf("------------------------------------------");
+       for(j=97;j<=122;j++)
+       {
+	printf("\nLower case alphabets are:%c\n",j);
+       }
+}
+
