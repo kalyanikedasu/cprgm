5d06556a4d24d12be86af14231ce6a079b9e139f
kalyanikedasu <kalyanikedasu3@gmial.com>
2025-07-28 17:32:25 +0530

    test

---
+++ factorial/basic.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int i,n,fact=1;
+	printf("Enter number:\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+		fact=fact*i;
+	printf("Factorial of %d is:%d \n",n,fact);
+}
