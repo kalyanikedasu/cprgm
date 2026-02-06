56ca2d93633598ea0ee2af5f665001b7c1e913a2
kalyanikedasu <kalyanikedasu3@gmial.com>
2026-02-06 18:17:12 +0530

    6feb update

---
+++ updates.c/update_06feb.c
@@ -0,0 +1,7 @@
+-->Tried led blinking in baremetal
+-->Initially led glowed but didn't blink after regular intervals
+-->Then blinked led with delay at regular time intervals
+-->Didn't work previously due to,
+	*incorrect gpio addresses
+	*small delay
+	*dim blink
