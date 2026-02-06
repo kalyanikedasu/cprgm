c24a0cce7162061931666c01cec01b6b0faff8bd
kalyanikedasu <kalyanikedasu3@gmial.com>
2026-02-06 18:22:41 +0530

    6feb update

---
+++ updates.c/update_6feb.c
@@ -0,0 +1,5 @@
+--> Tried LED blinking in bare-metal on QCC74x
+--> Initially LED glowed but did not blink at regular intervals  
+--> Fixed issues by correcting GPIO register addresses and increasing delay 
+--> Final result: LED blinking properly with clear ON/OFF timing
+
